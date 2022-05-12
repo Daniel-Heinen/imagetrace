840685e67fec1d313da45b57755412458ddb80bd
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-12 08:05:38 +0000

    fix: correct data validation

---
+++ src/geo_module_9.py
@@ -0,0 +1,14 @@
+"""Geolocation module 9 for imagetrace"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer9:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2025-08-07
+# Modified 2025-09-01
